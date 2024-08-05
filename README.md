55a74f165dc45d89446a4127b551bbb6803955d3
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-08-05 23:06:54 +0900

    add README.md and renew unixtime2humantime.py
    only using command line to convert unix time to human time

---
+++ README.md
@@ -0,0 +1,17 @@
+# PXcsv2USERcsv
+
+## 概要
+pixhawkのログのtimestampを日本時間にサクッと置換するやつ。  
+csvヘッダーの"timestamp"列を変換するので別にpixhawkじゃなくてもいける（はず）  
+
+## HOW TO USE
+
+```bash
+# リポジトリをクローン
+git clone https://github.com/Tail-BackShape/PXcsv2USERcsv.git
+
+# ディレクトリに移動
+cd PXcsv2USERcsv
+
+# 依存関係をインストール
+python unixtime2humantime input.csv #任意のcsv
--- unixtime2humantime.py
+++ unixtime2humantime.py
@@ -3,49 +3,42 @@ import dask.dataframe as dd
 import datetime
 import shutil
 import os
-import glob
+import sys
 
 
 def unixtime2humantime(read_filepath):
-
+    # Pandasの表示オプションを設定
     pd.set_option("display.max_rows", None)
     pd.set_option("display.max_columns", None)
     pd.set_option("display.width", None)
     pd.options.display.float_format = "{:.15f}".format
 
-    conv_dir = "./conv_csv"
-    if not os.path.exists(conv_dir):
-        os.makedirs(conv_dir)
-
-    raw_dir = "./raw_csv"
-    if not os.path.exists(raw_dir):
-        os.makedirs(raw_dir)
-
-    write_filepath = "./conv_csv/conv_" + os.path.basename(read_filepath)
+    # 書き込み用のファイルパスを設定
+    write_filepath = "conv_" + os.path.basename(read_filepath)
     shutil.copyfile(read_filepath, write_filepath)
 
+    # Dask DataFrameを読み込む
     ddf = dd.read_csv(write_filepath)
 
+    # timestamp列をフォーマット変換
     ddf["timestamp"] = ddf["timestamp"].apply(
-        lambda ts: datetime.datetime.fromtimestamp(ts),
-        meta=("timestamp", "datetime64[ns]"),
+        lambda ts: datetime.datetime.fromtimestamp(ts).strftime('%Y/%m/%d %H:%M:%S.%f')[:-3],
+        meta=("timestamp", "object"),
     )
     ddf = ddf.compute()
 
+    # 結果をCSVファイルに保存
     ddf.to_csv(write_filepath, index=False)
 
+    # 結果を表示
     print("\nInput file:\t", read_filepath)
     print("Output file:\t", write_filepath)
 
 
 if __name__ == "__main__":
-    print("unixtime2humantime.py\n")
-
-    file_list = glob.glob("./raw_csv/*.csv")
-    file_list = [x.replace("\\", "/") for x in file_list]
-    print("\n".join(file_list))
-    read_filepath = str(input("select csv file>"))
-
-    unixtime2humantime(read_filepath)
-
-    input("Enter any key...>")
+    if len(sys.argv) < 2:
+        print("Usage: python unixtime2humantime.py <input_csv_file>")
+    else:
+        read_filepath = sys.argv[1]
+        print("unixtime2humantime.py\n")
+        unixtime2humantime(read_filepath)
